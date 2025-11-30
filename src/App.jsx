0f71e1b940baf0f87d25f43adb501e90a7ee0980
Mfonisorobert <mfonisorobert1@gmail.com>
2025-11-30 22:33:05 +0100

    dhf

--- src/App.jsx
+++ src/App.jsx
@@ -240,7 +240,7 @@ if (!hasStarted) {
       <h3 className="text-2xl font-bold text-center text-blue-950 mb-2">
       {/* <h2 className="text-2xl font-bold text-center text-blue-950 mb-2">THIRD TERM EXAM</h2> */}
       {/* {showScore ? studentName : "HOW WELL DO YOU KNOW SUN DREAM?"} */}
-      {showScore ? studentName : "YEAR 12 - LITERATURE"}
+      {showScore ? studentName : "YEAR 10 - GOVERNMENT"}
       </h3>
 
       
@@ -252,7 +252,7 @@ if (!hasStarted) {
         <>
         <p className="text-1xl text-green-800 -mb-2  text-center"><span className="font-medium">{studentName}</span></p>
           
-          <Timer duration={2100} onTimeUp={handleTimeUp} /> {/* 5 minutes timer */}
+          <Timer duration={1800} onTimeUp={handleTimeUp} /> {/* 5 minutes timer */}
           {questions.length > 0 && (
             <Question
               questionObj={questions[currentQIndex]}
